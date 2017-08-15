e63e5abf741b1518a1cc222c7faec3b0e0334a64
Thys Meintjes <sthysel@gmail.com>
2017-08-16 01:12:02 +0800

    use wget ffs

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -1,11 +1,10 @@
 import os
 import sys
-from pprint import pformat
+import wget
 
 import click
-import requests
 
-SDK_FOLDER = '1_07_07'
+SDK_FOLDER = '01_07_07'
 SDK_VERSION = '1.07.07'
 SDK_DEST = './movidius/sdk/'
 DOCS_DEST = './movidius/docs/'
@@ -38,27 +37,6 @@ def get_sources(folder=SDK_FOLDER, version=SDK_VERSION, sdk_destination=SDK_DEST
     return sources
 
 
-def get_filename_from_response(response, verbose=0):
-    """
-    :param response: The response object from requests
-    :param verbose: Chattiness
-    :return:
-    """
-
-    if verbose >= 2:
-        click.echo(pformat(dict(**response.headers)))
-        for r in response.history:
-            click.echo(pformat(dict(**r.headers)))
-
-    try:
-        location = response.history[0].headers.get('Location')
-        filename = location.split('/')[-1]
-    except:
-        raise ValueError('Could not find a filename')
-    else:
-        return filename
-
-
 def get_filesize(response):
     try:
         return int(response.headers.get('Content-Length'))
@@ -75,40 +53,39 @@ def remove_existing_file(cache_file_name, overwrite):
             sys.exit()
 
 
-def fetch(source, dest, overwrite=False):
+def fetch(source_url, dest_directory, dest_filename, overwrite=False):
     """ Fetch sources """
 
-    os.makedirs(dest, exist_ok=True)
+    click.echo('Fetching {}'.format(source_url))
+    os.makedirs(dest_directory, exist_ok=True)
 
-    click.echo('Contacting server...')
-    response = requests.get(source, stream=True)
-
-    fname = get_filename_from_response(response)
-    file_size = get_filesize(response)
-    cache_file_name = os.path.join(dest, fname)
-    remove_existing_file(cache_file_name, overwrite)
-
-    click.echo('Fetching {}'.format(cache_file_name))
-    if response.status_code == 200:
-        with open(cache_file_name, 'wb') as f:
-            with click.progressbar(length=file_size) as bar:
-                for chunk in response.iter_content(chunk_size=1024):
-                    f.write(chunk)
-                    bar.update(len(chunk))
+    _file_name = os.path.join(dest_directory, dest_filename)
+    remove_existing_file(_file_name, overwrite)
+    wget.download(source_url, _file_name)
 
 
 @click.command()
-@click.option('--version', default=SDK_VERSION, help='The movidius SDK version', show_default=True)
+@click.option('--sdk-version', default=SDK_VERSION, help='The movidius SDK version', show_default=True)
 @click.option('--folder', default=SDK_FOLDER, help='The movidius SDK folder on server', show_default=True)
 @click.option('--sdk-destination', default=SDK_DEST, help='The movidius SDK target folder here', show_default=True)
 @click.option('--docs-destination', default=DOCS_DEST, help='The movidius SDK documentation folder here', show_default=True)
-def cli(version, folder, sdk_destination, docs_destination):
+@click.option('--overwrite', is_flag=True, default=False, help='Overwrite existing files', show_default=True)
+def cli(sdk_version, folder, sdk_destination, docs_destination, overwrite):
     """ bootstrap deepshtick development environment"""
 
     for name, info in get_sources(
-            version=version,
+            version=sdk_version,
             folder=folder,
             sdk_destination=sdk_destination,
             docs_destination=docs_destination).items():
         click.echo('Fetching {}'.format(name))
-        fetch(info['source'], info['dest'])
+        source = info['source']
+        dest = info['dest']
+
+        filename = source.split('/')[-1]
+
+        fetch(source_url=source, dest_directory=dest, dest_filename=filename, overwrite=overwrite)
+
+
+if __name__ == '__main__':
+    cli()
